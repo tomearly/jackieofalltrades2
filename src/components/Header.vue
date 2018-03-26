d0896a8228c56d715ed2f852749177e924df146b
Your Name <you@example.com>
2018-03-26 21:07:24 +0100

    a

---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+      {{message}}
+    </div>
+</template>
+
+<script>
+export default {
+  props: ['message'],
+  data () {
+    return {
+    }
+  },
+  name: 'Header'
+}
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+      <Header
+        message='test'
+      >
+      </Header>
+    </div>
+</template>
+
+<script>
+import Header from './Header'
+
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  components: {
+    Header
+  }
+}
+</script>
