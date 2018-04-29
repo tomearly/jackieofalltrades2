5ea00cbf9253177b11037bc6ee6d2fda4d37719a
Tom Early <tom.early@gmail.com>
2018-04-29 07:29:39 +0100

    Added footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class='footer'>
+      <span
+        class='vertical-align-content'
+        >&copy; tom early 2018
+      </span>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
+
+<style scoped>
+.footer {
+    background-color: white;
+    bottom: 0px;
+    position: absolute;
+    text-align: center;
+    width: 100%;
+}
+</style>
